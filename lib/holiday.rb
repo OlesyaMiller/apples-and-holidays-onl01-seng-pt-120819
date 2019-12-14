b1318f03ea282a8cd7760a8f30e7862dfbb2dd32
Olesya Miller <olesyam.miller@gmail.com>
2019-12-14 02:44:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
    puts "#{k1.to_s.capitalize}:"
    
    v1.each do |k2, v2|
-     puts "#{k2.to_s.split("_").map { |x| x.capitalize }.join(" ") }: #{v2.join(", ")}" 
+     puts " #{k2.to_s.split("_").map { |x| x.capitalize }.join(" ") }: #{v2.join(", ")}" 
    end
  end
 end
